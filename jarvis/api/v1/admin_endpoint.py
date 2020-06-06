1a6282d612a375f9a51d6afcaff7951e33c7e648
christian-miljkovic <christian.miljkovic@gmail.com>
2020-06-06 19:06:57 -0400

    Adding update and delete endpoints

--- jarvis/api/v1/admin_endpoint.py
+++ jarvis/api/v1/admin_endpoint.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 from jarvis.db.database import DataBase, get_database
 from jarvis.core.models import Item
 from jarvis.models import Beer, Liquor, Wine
@@ -13,6 +14,43 @@ async def add_item_to_db(
 ):
     type_mapping = {"beer": Beer, "liquor": Liquor, "wine": Wine}
     async with db.pool.acquire() as conn:
-        type_converted_item = type_mapping.get(drink_type)(**item.dict())
-        added_item = await crud.create_item(conn, type_converted_item)
-        return added_item
+        try:
+            type_converted_item = type_mapping.get(drink_type)(**item.dict())
+            added_item = await crud.create_item(conn, type_converted_item)
+            return added_item
+        except UserWarning as warning:
+            return JSONResponse(
+                status_code=status.HTTP_202_ACCEPTED, content=str(warning)
+            )
+
+
+@router.post("/update/{drink_type}")
+async def update_item_to_db(
+    drink_type: str, item: Item, db: DataBase = Depends(get_database)
+):
+    type_mapping = {"beer": Beer, "liquor": Liquor, "wine": Wine}
+    async with db.pool.acquire() as conn:
+        try:
+            type_converted_item = type_mapping.get(drink_type)(**item.dict())
+            updated_item = await crud.update_item(conn, type_converted_item)
+            return updated_item
+        except UserWarning as warning:
+            return JSONResponse(
+                status_code=status.HTTP_202_ACCEPTED, content=str(warning)
+            )
+
+
+@router.post("/delete/{drink_type}")
+async def delete_item_to_db(
+    drink_type: str, item: Item, db: DataBase = Depends(get_database)
+):
+    type_mapping = {"beer": Beer, "liquor": Liquor, "wine": Wine}
+    async with db.pool.acquire() as conn:
+        try:
+            type_converted_item = type_mapping.get(drink_type)(**item.dict())
+            deleted_item = await crud.delete_item(conn, type_converted_item)
+            return deleted_item
+        except UserWarning as warning:
+            return JSONResponse(
+                status_code=status.HTTP_202_ACCEPTED, content=str(warning)
+            )
--- jarvis/crud/__init__.py
+++ jarvis/crud/__init__.py
@@ -1,3 +1,3 @@
 from .items import *
 
-__all__ = [get_all_item_by_type, create_item]
+__all__ = [get_all_item_by_type, create_item, update_item, delete_item]
